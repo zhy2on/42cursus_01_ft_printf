bf9ad04df856847ad0b8f6fe3a7e402d434f4c0c
zhy2on <dhwlgus5940@gmail.com>
2021-07-25 05:42:45 +0900

    add set_nbr_info

--- ft_printf.h
+++ ft_printf.h
@@ -43,14 +43,19 @@ int		ft_printf(const char *format, ...);
 int		parse_format(va_list ap, char *format);
 void	init_info(t_info *info);
 void	check_info(char *format, t_info *info, int i);
+int		print_spec(va_list ap, t_info *info);
 
 /*
-*** FT_PRINT ***
+*** FT_PRINT %c, %s ***
 */
-int		print_spec(va_list ap, t_info *info);
 int		print_char(int c, t_info *info);
 int		print_string(char *str, t_info *info);
+
+/*
+*** FT_PRINT %d, %u, %x, %X ***
+*/
 int		print_nbr(unsigned long long nbr, t_info *info);
+void	set_nbr_info(unsigned long long nbr, t_info *info);
 int		put_nbr_base(unsigned long long nbr, t_info *info);
 void	put_nbr_sub(unsigned long long nbr, char *base, int len, int *ret);
 int		nbr_base_len(unsigned long long nbr, t_info *info);
